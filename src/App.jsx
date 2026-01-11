db1930f7fbc2b44711eb012d41cde453d28dc7d6
kalai02484 <kalai02484@gmail.com>
2026-01-11 10:50:45 +0530

    React Router dom added and updated base code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart";
+import Error from "./pages/Error";
+import ProductList from "./pages/ProductList";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  )
-}
+    <>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<ProductList/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="*" element={<Error/>} />          
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
